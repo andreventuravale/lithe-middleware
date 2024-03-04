8593d6a7020f8d0464b7194040d16a14cd1dfc40
Andre Vale <andreventuravale@gmail.com>
2024-03-04 13:38:04 -0300

    .eslintrc.cjs .gitignore .prettierignore .prettierrc LICENSE.md coverage.txt package.json pnpm-lock.yaml src/main.test.ts src/main.ts tsconfig.esm.json tsconfig.json

---
+++ src/main.test.ts
@@ -0,0 +1,254 @@
+import {
+  AnonymousMiddleware,
+  Middleware,
+  makePipeline,
+  passAlong
+} from './main.js'
+import test from 'ava'
+
+test('linear', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (text) => await next(text + ' b'),
+    (next) => async (text) => await next(text + 'a'),
+    (next) => async (text) => await next(text + 'r')
+  ])
+
+  const request = pipeline()
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+})
+
+test('an empty pipeline returns the original output', async (t) => {
+  const pipeline = makePipeline([])
+
+  const request = pipeline()
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo')
+})
+
+test('nested', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input + 'a'),
+    (next) => async (input) =>
+      await next(
+        await makePipeline([
+          (next) => async (input) => await next(input + 'b'),
+          (next) => async (input) => await next(input + 'c')
+        ])()(input)
+      )
+  ])
+
+  const request = pipeline()
+
+  const reply = await request('')
+
+  t.deepEqual(reply, 'abc')
+})
+
+test('named links', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input + ' b'),
+    ['adds a', (next) => async (output) => await next(output + 'a')],
+    ['adds r', (next) => async (output) => await next(output + 'r')]
+  ])
+
+  const request = pipeline()
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+})
+
+test('places a middleware prior to', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input + ' b'),
+    ['adds r', (next) => async (input) => await next(input + 'r')]
+  ])
+
+  const request = pipeline([
+    ['before', 'adds r', (next) => async (input) => await next(input + 'a')]
+  ])
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+})
+
+test('adds a middleware subsequent to', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input + ' b'),
+    ['adds a', (next) => async (input) => await next(input + 'a')]
+  ])
+
+  const request = pipeline([
+    ['after', 'adds a', (next) => async (input) => await next(input + 'r')]
+  ])
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+})
+
+test('produces an error if the modification reference cannot be found', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input + ' b'),
+    ['adds r', (next) => async (input) => await next(input + 'r')]
+  ])
+
+  const request = pipeline([
+    ['before', 'foo bar', (next) => async (input) => await next(input + 'a')]
+  ])
+
+  await t.throwsAsync(
+    async () => {
+      await request('foo')
+    },
+    { message: 'could not find middleware named: "foo bar"' }
+  )
+})
+
+test('modifications do not affect the original input list', async (t) => {
+  const list: Middleware[] = [
+    (next) => async (input) => await next(input + ' b'),
+    ['adds r', (next) => async (input) => await next(input + 'r')]
+  ]
+
+  const pipeline = makePipeline(list)
+
+  t.truthy(list.length === 2)
+
+  const request = pipeline([
+    ['before', 'adds r', (next) => async (input) => await next(input + 'a')]
+  ])
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+
+  t.true(list.length === 2)
+})
+
+test('normal pipelines do not affect the original input list', async (t) => {
+  const list: AnonymousMiddleware[] = [
+    (next) => async (input) => await next(input + ' b'),
+    (next) => async (input) => await next(input + 'a'),
+    (next) => async (input) => await next(input + 'r')
+  ]
+
+  const pipeline = makePipeline(list)
+
+  t.truthy(list.length === 3)
+
+  const request = pipeline()
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+
+  t.truthy(list.length === 3)
+})
+
+test('modifications are processed in the same sequence as they are provided', async (t) => {
+  const list: Middleware[] = [
+    ['first', (next) => async (input) => await next(input + ' b')]
+  ]
+
+  const pipeline = makePipeline(list)
+
+  const request = pipeline([
+    ['after', 'first', (next) => async (input) => await next(input + 'a')],
+    ['after', 'first', (next) => async (input) => await next(input + 'r')]
+  ])
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo bar')
+})
+
+test('modifications are processed in the same sequence as they are provided ( extended case )', async (t) => {
+  const list: Middleware[] = [
+    ['1st', (next) => async (input) => await next(input + '1')],
+    ['2nd', (next) => async (input) => await next(input + '2')],
+    ['3rd', (next) => async (input) => await next(input + '3')],
+    ['4th', (next) => async (input) => await next(input + '4')]
+  ]
+
+  const pipeline = makePipeline(list)
+
+  const request = pipeline([
+    ['after', '1st', (next) => async (input) => await next(input + 'a')],
+    ['after', '4th', (next) => async (input) => await next(input + 'd')],
+    ['before', '3rd', (next) => async (input) => await next(input + 'c')],
+    ['after', '1st', (next) => async (input) => await next(input + 'b')]
+  ])
+
+  const reply = await request('')
+
+  t.deepEqual(reply, '1ab2c34d')
+})
+
+test('substituting links', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input),
+    ['foo', (next) => async (input) => await next(input + 'foo')],
+    ['bar', (next) => async (input) => await next(input + ' bar')]
+  ])
+
+  t.deepEqual(await pipeline()(''), 'foo bar')
+
+  t.deepEqual(
+    await pipeline([
+      ['replace', 'foo', (next) => async (input) => await next(input + 'qux')],
+      [
+        'replace',
+        'bar',
+        (next) => async (input) => await next(input + ' waldo')
+      ]
+    ])(''),
+    'qux waldo'
+  )
+})
+
+test('bypassing middleware processing', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input),
+    ['1st', (next) => async (input) => await next(input + '1')],
+    ['2nd', (next) => async (input) => await next(input + ' 2')],
+    ['3rd', (next) => async (input) => await next(input + ' 3')]
+  ])
+
+  t.deepEqual(await pipeline()(''), '1 2 3')
+
+  t.deepEqual(await pipeline([['skip', '2nd']])(''), '1 3')
+})
+
+test('the passAlong helper', async (t) => {
+  const pipeline = makePipeline([() => passAlong])
+
+  const request = pipeline()
+
+  const reply = await request('foo')
+
+  t.deepEqual(reply, 'foo')
+})
+
+test('appending links', async (t) => {
+  const pipeline = makePipeline([
+    (next) => async (input) => await next(input),
+    ['1st', (next) => async (input) => await next(input + '1')],
+    ['2nd', (next) => async (input) => await next(input + ' 2')],
+    ['3rd', (next) => async (input) => await next(input + ' 3')]
+  ])
+
+  t.deepEqual(
+    await pipeline([
+      (next) => async (input) => await next(input + ' 4'),
+      (next) => async (input) => await next(input + ' 5')
+    ])(''),
+    '1 2 3 4 5'
+  )
+})
---
+++ src/main.ts
@@ -0,0 +1,104 @@
+export type ChainHandler<Input = unknown, Output = unknown> = (
+  input: Input
+) => Promise<Output>
+
+export type Next = (input: unknown) => Promise<unknown>
+
+export type AnonymousMiddleware<Input = unknown> = (
+  next: Next
+) => ChainHandler<Input>
+
+export type NamedMiddleware<Input = unknown> = [
+  string,
+  AnonymousMiddleware<Input>
+]
+
+export type Middleware<Input = unknown> =
+  | AnonymousMiddleware<Input>
+  | NamedMiddleware<Input>
+
+export type PipelineModifications<Input = unknown> =
+  | ['before' | 'after' | 'replace', name: string, Middleware<Input>]
+  | ['skip', name: string]
+  | Middleware<Input>
+
+export type Pipeline<Input = unknown> = <Output = unknown>(
+  modifications?: PipelineModifications[]
+) => ChainHandler<Input, Output>
+
+export const makePipeline = <Input>(links: Middleware[]): Pipeline<Input> => {
+  const pipeline: Pipeline<Input> = (modifications = []) => {
+    return async (input: Input) => {
+      const list = modify(links, modifications)
+
+      const next: Next = async (input) => {
+        const current = list.shift()
+
+        if (current) {
+          return await invoke(current, next, input)
+        }
+
+        return input
+      }
+
+      const head = list.shift()
+
+      if (head) {
+        return await invoke(head, next, input)
+      }
+
+      return input
+
+      async function invoke(node, next, input) {
+        if (typeof node === 'function') {
+          return await node(next)(input)
+        } else {
+          return await node[1](next)(input)
+        }
+      }
+    }
+  }
+
+  return pipeline
+}
+
+export const passAlong: Next = async (input) => input
+
+function modify(
+  baseList: readonly Middleware[],
+  modificationList: readonly PipelineModifications[]
+): Middleware[] {
+  const modifications = modificationList.slice(0)
+
+  modifications.reverse()
+
+  const base = baseList.slice(0)
+
+  const initialLength = base.length
+
+  for (const modification of modifications) {
+    if (typeof modification === 'function') {
+      base.splice(initialLength, 0, modification)
+    } else {
+      const [action, name, middleware] = modification
+
+      const index = base.findIndex(
+        (existing) => typeof existing !== 'function' && name === existing[0]
+      )
+
+      if (index < 0) {
+        throw new Error(`could not find middleware named: "${name}"`)
+      }
+
+      if (action === 'replace') {
+        base[index][1] = middleware
+      } else if (action === 'skip') {
+        base.splice(index, 1)
+      } else {
+        base.splice(action === 'before' ? index : index + 1, 0, middleware)
+      }
+    }
+  }
+
+  return base
+}
